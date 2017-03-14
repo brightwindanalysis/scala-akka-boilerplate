f05adb536ec2024ce218b4d5a1abb684c1e9b37c
niqdev <niqdev@gmail.com>
2017-03-14 23:51:25 +0000

    fix status test

--- src/test/scala/com/brightwindanalysis/http/RoutesSpec.scala
+++ src/test/scala/com/brightwindanalysis/http/RoutesSpec.scala
@@ -10,12 +10,17 @@ import akka.http.scaladsl.testkit.ScalatestRouteTest
 import com.brightwindanalysis.Main.routes
 import org.scalatest.{Matchers, WordSpec}
 
+// scalastyle:off underscore.import
+import io.circe.generic.auto._
+import io.circe.syntax._
+// scalastyle:on underscore.import
+
 class RoutesSpec extends WordSpec with Matchers with ScalatestRouteTest {
 
   "routes /status" should {
     "return OK for GET requests to the root path" in {
       Get("/status") ~> routes ~> check {
-        responseAs[String] shouldEqual "OK"
+        responseAs[String] shouldEqual Status("OK").asJson.noSpaces
       }
     }
   }
