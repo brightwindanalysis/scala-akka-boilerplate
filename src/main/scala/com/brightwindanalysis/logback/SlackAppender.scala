ff76d7a2f6b053ffeb8340ef73efff2e2b2cd30e
niqdev <niqdev@gmail.com>
2017-04-11 21:32:04 +0100

    slack appender post message

--- src/main/scala/com/brightwindanalysis/logback/SlackAppender.scala
+++ src/main/scala/com/brightwindanalysis/logback/SlackAppender.scala
@@ -6,17 +6,23 @@
 
 package com.brightwindanalysis.logback
 
+import java.time.ZonedDateTime
+
 import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.{AppenderBase, Layout}
+import dispatch.Defaults.executor
+import dispatch.{Http, url}
 
 import scala.beans.BeanProperty
+import scala.util.{Failure, Success}
 
 class SlackAppender extends AppenderBase[ILoggingEvent] {
 
+  @BeanProperty var applicationName: String = _
   @BeanProperty var webhookUrl: String = _
   @BeanProperty var layout: Layout[ILoggingEvent] = _
 
-  private[this] var isValidWebhookUrl: Boolean = true
+  private[this] var isValidWebhookUrl: Boolean = _
 
   override def start(): Unit = {
     isValidWebhookUrl = Option(webhookUrl) match {
@@ -32,8 +38,17 @@ class SlackAppender extends AppenderBase[ILoggingEvent] {
 
   override def append(event: ILoggingEvent): Unit = {
     if (isValidWebhookUrl) {
-      // TODO
-      addError(s"TODO ${event.getFormattedMessage}")
+
+      val eventLog = s"""{"text": "${ZonedDateTime.now}\n$applicationName\n${event.getFormattedMessage}"}"""
+      val request = url(webhookUrl)
+        .POST
+        .setContentType("application/json", "UTF-8") << eventLog
+
+      Http(request)
+        .onComplete {
+          case Success(_) => addInfo("slack notification succeed")
+          case Failure(error) => addError("slack notification failed", error)
+        }
     }
   }
 
